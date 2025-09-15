13778b6c3f6679821c21382b95feec4fb6576f05
unknown <adewalegithub240@gmail.com>
2025-09-15 02:36:10 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//Navbar
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  const toggler = document.getElementById("toggler");
+  menu.classList.toggle("active");
+  if (menu.classList.contains("active")) {
+    toggler.classList.remove("fa-bars"); // Remove hamburger icon
+    toggler.classList.add("fa-times"); // Add close icon
+  } else {
+    toggler.classList.remove("fa-times"); // Remove close icon
+    toggler.classList.add("fa-bars"); // Add hamburger icon
+  }
+}
+//Navbar END
+// Change navbar style on scroll
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector("nav");
+  if (window.scrollY > 50) {
+    // adjust scroll amount
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
